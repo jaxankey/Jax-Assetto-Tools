8d44bc6a224db56bbe3a5e407f9976258317ce35
Ubuntu <ubuntu@ip-172-31-43-165.us-east-2.compute.internal>
2022-01-24 14:52:16 -0500

    helpers

---
+++ helpers/discord-send.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import discord, sys, time
+
+# The supplied init file path (argv[0]) will define url_webhook, message_id, and message
+url_webhook  = None
+message      = None
+delete_after = None
+exec(open(sys.argv[1], 'r', encoding="utf8").read())
+
+# Create the webhook
+webhook = discord.Webhook.from_url(url_webhook, adapter=discord.RequestsWebhookAdapter())
+
+# Sending by embed makes it prettier and larger
+e = discord.Embed()
+e.color       = 15548997 # Red
+e.description = message.strip()
+
+# Send it!
+id = webhook.send('', embeds=[e], wait=True)
+
+# If we're supposed to delete...
+if delete_after:
+  time.sleep(60*delete_after)
+  webhook.delete_message(id)
+
