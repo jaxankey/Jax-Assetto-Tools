449659e9e555e7f54e00b9ec7a65974a28cfa46b
jaxankey <jack.sankey@gmail.com>
2022-01-28 23:41:19 -0500

    Update compile.bat

--- uploader/compile.bat
+++ uploader/compile.bat
@@ -1,5 +1,5 @@
 call C:/Users/Jack/Miniconda3/Scripts/activate.bat C:/Users/Jack/Miniconda3
-pyinstaller --onefile --distpath . uploader.py
+pyinstaller --onefile --distpath . uploader.pyw
 del uploader.spec
 rmdir /s/q build
 rmdir /s/q __pycache__
