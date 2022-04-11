a6ac52de536b0dc60e56f984665c5a9861cbbf4b
Jack <jack.sankey@gmail.com>
2022-04-11 09:12:57 -0400

    message sending

--- helpers/discord-send.py
+++ helpers/discord-send.py
@@ -3,9 +3,10 @@
 import discord, sys, time
 
 # The supplied init file path (argv[0]) will define url_webhook, message_id, and message
-url_webhook  = None
-message      = None
-delete_after = None
+url_webhook    = None
+message_header = None
+message        = None
+delete_after   = None
 exec(open(sys.argv[1], 'r', encoding="utf8").read())
 
 # Create the webhook
@@ -17,7 +18,8 @@ e.color       = 15548997 # Red
 e.description = message.strip()
 
 # Send it!
-message = webhook.send('', embeds=[e], wait=True)
+if message_header == None: message_header = ''
+message = webhook.send(message_header, embeds=[e], wait=True)
 
 # If we're supposed to delete...
 if delete_after:
