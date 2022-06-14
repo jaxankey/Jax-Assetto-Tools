76fcb7ecadf77e0c7439301d087ee8eb5dc5d1d6
jaxankey <jack.sankey@gmail.com>
2022-06-14 17:31:18 -0400

    Update monitor.py

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -917,7 +917,7 @@ class Monitor:
 
                 # Stylize the registration link
                 reg_string1 = nametime1 # Bottom registration
-                reg_string2 = ':point_right:' + reg_string2 + ' :point_left:\n'+nametime2 # Top time stamp
+                reg_string2 = reg_string2 + '\n'+nametime2 # Top time stamp
 
 
         # Track name
@@ -932,7 +932,7 @@ class Monitor:
             title = '[' + title +']('+url_event_info+')'
 
         # Assemble the message body
-        body1 = venue_header + '**__'+title+'__**'
+        body1 = ':point_right: ' + venue_header + '**__'+title+'__** :point_left:'
 
         # Subheader
         body1 = body1 + reg_string2 + venue_subheader
