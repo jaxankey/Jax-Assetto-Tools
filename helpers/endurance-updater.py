cacd30374d0b284c43e66ede23b4998013004ccf
jaxankey <jack.sankey@gmail.com>
2022-12-12 16:31:38 -0500

    Update endurance-updater.py

--- helpers/endurance-updater.py
+++ helpers/endurance-updater.py
@@ -92,9 +92,9 @@ for n in range(len(list(c['Events'][0]['EntryList'].keys()))):
         skin = ''
         ids = ''
 
-    # Get the internal UUID
-    uuid = c['Classes'][0]['Entrants']['CAR_%d'%(n+1)]['InternalUUID']
-    c['Events'][0]['EntryList']['CAR_%d'%(n  )]['InternalUUID'] = n
+    # Get the internal UUID THIS CAUSES CRASHES; THEY NEED TO BE UNIQUE
+    # uuid = c['Classes'][0]['Entrants']['CAR_%d'%(n+1)]['InternalUUID']
+    # c['Events'][0]['EntryList']['CAR_%d'%(n  )]['InternalUUID'] = n
 
     c['Classes'][0]['Entrants']['CAR_%d'%(n+1)]['PitBox'] = n
     c['Events'][0]['EntryList']['CAR_%d'%(n  )]['PitBox'] = n
